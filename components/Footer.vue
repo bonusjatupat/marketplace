56c9c6ff9897743aaacc038aa146b5a0e61c2260
bonusjatupat <jatupat.tm@gmail.com>
2020-06-07 21:33:04 +0700

    [ADD] Footer, Products' Cards

---
+++ components/Footer.vue
@@ -0,0 +1,94 @@
+<template>
+  <div id="Footer">
+      <footer>
+            <div class="container-fluid padding">
+                <div class="row">
+                    <div class="col-md-2 content">
+                        <p class="title font-weight-bold">Help Center</p>
+                        <p>Track My Order</p>
+                        <p>How to buy Our Products</p>
+                        <p>Return & Refund</p>
+                        <p>Privacy Policy</p>
+                        <p>Cheapest Guaranteed</p>
+                    </div>
+                     <div class="col-md-2 content">
+                        <p class="title font-weight-bold">About Us</p>
+                        <p>About NocNoc.com</p>
+                    </div>
+                    <div class="col-md-2 content">
+                        <p class="title font-weight-bold">Contact Us</p>
+                        <p>Become a Seller</p>
+                        <p>Become an Installer</p>
+                        <p>Find Installers</p>
+                        <p>Chat with Us</p>
+                    </div>
+                    <div class="col-md-2 content">
+                        <p class="title font-weight-bold">Connect With Us</p>
+                        <ion-icon name="logo-facebook" class="icon-social"></ion-icon>
+                        <ion-icon name="logo-youtube" class="icon-social"></ion-icon>
+                        <ion-icon name="logo-twitter" class="icon-social"></ion-icon>
+                        <ion-icon name="logo-whatsapp" class="icon-social"></ion-icon>
+                        <p class="title font-weight-bold">Payment Methods</p>
+                        <ion-icon name="card" class="icon-social"></ion-icon>
+                        <ion-icon name="cash" class="icon-social"></ion-icon>
+                        <ion-icon name="wallet" class="icon-social"></ion-icon>
+                    </div>
+                    <div class="col-md-2 text-right content">
+                        <p class="title font-weight-bold">Subscribe to Our Newsletter</p>
+                            <div class="input-group ml-auto">
+                                <input type="text" class="form-control" placeholder="Email Address" />
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary" type="button" id="search-button">
+                                        Follow
+                                    </button>
+                                </div>
+                            </div>
+                        <p>Monday - Friday 8:00 - 22:00</p>
+                        <p>Saturday - Sunday 9:00 - 20:00</p>
+                    </div>
+                    <div class="col-12 footer-bottom">
+                        <p class="text-center">BetterBe Marketplace Company Limited 162/1-2,168/10 Pracha Rat Sai 2 Rd Bangsue, Bangkok 10800</p>
+                    </div>
+                </div>
+            </div>
+      </footer>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style scoped>
+footer {
+    background-color: #110C34;
+    color: #bebdbd;
+    font-size: 13px;
+}
+.title {
+    color: #FFFFFF;
+    margin-top: 3rem;
+}
+.content {
+    margin-left: 2rem;
+}
+.icon-social {
+    font-size: 25px;
+    margin-right: .5rem;
+    margin-bottom: .5rem;
+}
+.input-group {
+    margin-bottom: 1rem;
+}
+.footer-bottom {
+    background-color: rgb(8, 6, 26);
+    color: #FFFFFF;
+}
+.footer-bottom p {
+    margin-top: .5rem;
+    margin-bottom: .5rem;
+    font-size: 11px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,171 @@
 <template>
   <div id="Index">
     <PromotionSlide />
+    <div class="main-container">
+
+      <!-- NocNoc's Products! -->
+      <div class="container-fluid padding title">
+        <div class="row padding">
+          <div class="col-xs-12 col-sm-12 col-md-2">
+            <img src="../assets/images/choices.png" class="img-choices">
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-10">
+            <h2>NocNoc's Products!</h2>
+            <p>NocNoc's Highly Rated And Well-Priced Product That Is Ready For You</p>
+          </div>
+        </div>
+      </div>
+
+      <!-- Products' Cards -->
+      <div class="row row-cols-1 row-cols-md-5 product-cards">
+        <div class="col mb-4">
+          <div class="card h-100">
+            <img src="../assets/images/promotion1.png" class="card-img-top" alt="product">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-4">
+          <div class="card h-100">
+            <img src="../assets/images/promotion1.png" class="card-img-top" alt="product">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-4">
+          <div class="card h-100">
+            <img src="../assets/images/promotion1.png" class="card-img-top" alt="product">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-4">
+          <div class="card h-100">
+            <img src="../assets/images/promotion1.png" class="card-img-top" alt="product">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-4">
+          <div class="card h-100">
+            <img src="../assets/images/promotion1.png" class="card-img-top" alt="product">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-4">
+          <div class="card h-100">
+            <img src="../assets/images/promotion1.png" class="card-img-top" alt="product">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+        </div>
+
+        <div class="col mb-4">
+          <div class="card h-100">
+            <img src="../assets/images/promotion1.png" class="card-img-top" alt="product">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-4">
+          <div class="card h-100">
+            <img src="../assets/images/promotion1.png" class="card-img-top" alt="product">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-4">
+          <div class="card h-100">
+            <img src="../assets/images/promotion1.png" class="card-img-top" alt="product">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-4">
+          <div class="card h-100">
+            <img src="../assets/images/promotion1.png" class="card-img-top" alt="product">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-4">
+          <div class="card h-100">
+            <img src="../assets/images/promotion1.png" class="card-img-top" alt="product">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-4">
+          <div class="card h-100">
+            <img src="../assets/images/promotion1.png" class="card-img-top" alt="product">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -15,5 +180,23 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.main-container {
+  padding-left: 4rem;
+  padding-right: 4rem;
+  margin-top: 1rem;
+}
+.img-choices {
+  width: 200px;
+  height: 80px;
+}
+.title h2 {
+  margin-top: .5rem;
+  margin-bottom: 0rem;
+}
+.product-cards {
+  margin-top: 1rem;
+  margin-left: .1rem;
+  margin-right: .1rem;
+}
 </style>
